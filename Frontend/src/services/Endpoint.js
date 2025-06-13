cde8720bc8c0203dcfc20ca5f2150424ecd55ab5
Deepak Kumar <1172dipu@gmail.com>
2025-06-14 02:16:30 +0530

    fix dockerfile and Backend connections

--- Frontend/src/services/Endpoint.js
+++ Frontend/src/services/Endpoint.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-export const BaseUrl='http://localhost:5000'
+// export const BaseUrl='http://localhost:5000'
+export const BaseUrl='/api'
+
 const instance = axios.create({
     baseURL:BaseUrl,
     // headers: {
